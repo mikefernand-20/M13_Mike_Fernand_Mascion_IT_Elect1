29af269d948ff27306322185f1e63a347a0c2fee
mikefernand-20 <kaifalexo012@gmail.com>
2025-09-22 23:47:56 +0800

    Import ColorChangerApp and CounterApp;

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { ScrollView } from 'react-native';
+import ColorChangerApp from './ColorChangerApp';
+import CounterApp from './CounterApp';
+import Messenger from './Messenger';
+import NewsFeed from './NewsFeed';
+import styles from './styles';
+
+
+const App = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <Messenger />
+      <NewsFeed />
+      
+    </ScrollView>
+  );
+};
+
+export default App;
